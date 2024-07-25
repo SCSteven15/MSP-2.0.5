b02d9bc9ddab2ffb943aa03619ae65a5f2bb096f
SCSteven15 <tevinho408@gmail.com>
2024-07-25 21:13:39 +0000

    Mundo guardado 2024-07-25

--- server.py
+++ server.py
@@ -18,6 +18,25 @@
 # us-cal-1	      Estados unidos (California)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # No toques nada de aquí para abajo, puedes dañarlo
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
